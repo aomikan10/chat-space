4ebfc20aa5308d7586684ed1b109236366b56c31
aomikan10 <mm1021201@gmail.com>
2020-07-08 21:21:05 +0900

    Update.controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update] 
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :groups, only: [:new, :create]
 end
