c2a81955263aa5988deebd3ab14f5ae4594fa1ed
aomikan10 <mm1021201@gmail.com>
2020-07-14 15:44:58 +0900

    Update.messages_controller.rb

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,15 @@
 $(function(){
   $('.Form').on('submit', function(e) {
     e.preventDefault();
-    console.log(go);
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',  
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
